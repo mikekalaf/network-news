0e0b2631f908cb494c72569d1a1a51193aeb257e
David Schweinsberg <david.schweinsberg@gmail.com>
2018-04-09 17:00:02 -0700

    New EncodedWordEncoder

---
+++ NetworkNews/EncodedWordEncoder.h
@@ -0,0 +1,13 @@
+//
+//  EncodedWordEncoder.h
+//  NetworkNews
+//
+//  Created by David Schweinsberg on 4/9/18.
+//  Copyright © 2018 David Schweinsberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EncodedWordEncoder : NSObject
+
+@end
---
+++ NetworkNews/EncodedWordEncoder.m
@@ -0,0 +1,13 @@
+//
+//  EncodedWordEncoder.m
+//  NetworkNews
+//
+//  Created by David Schweinsberg on 4/9/18.
+//  Copyright © 2018 David Schweinsberg. All rights reserved.
+//
+
+#import "EncodedWordEncoder.h"
+
+@implementation EncodedWordEncoder
+
+@end
